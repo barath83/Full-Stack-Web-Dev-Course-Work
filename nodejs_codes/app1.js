232bed52896412a4e59d1a934bd729152051ff19
Barath Gopinath <barathgopi1699@gmail.com>
2020-10-02 10:58:59 +0530

    server req/res node code

--- nodejs_codes/app1.js
+++ nodejs_codes/app1.js
@@ -1,38 +1,21 @@
-const http = require('http');
+ 
+// importing file system module offered by nodeJs 
+const fs = require('fs');
 
-//http.createServer returns a server object
-//takes one function with two arguments req,res provided by nodejs
-const server = http.createServer((req,res)=> {
-    console.log('INCOMING REQUEST');
-    console.log(req.method,req.url);
+const user = "barath";
 
-    if(req.method == 'POST'){
-        
-        let body = '';
-
-        req.on('end',() => { 
-
-            const userName = body.split('=')[1];
-            res.end('<h1>'+ userName +'</h1>');
-        });
-
-        //req for a data event as we get a chunk of data in stream
-        req.on('data',(chunk) => {
-            body+=chunk;
-        });
+//takes three arguments
+//1.file name to be created, 2.data to be written, 3.callback function to handle errors
+fs.writeFile('user_data.txt','Name:'+user,(err)=>{
+    if(err){
+        console.log(err);
+        return;
     }
-    else{
-        res.setHeader('Content-Type','text/html');
-        res.end('<form method="POST"><input type="text" name="username"><button>Create User</button></form>');
-    }
-    //makes the response sent as plain text not as html
-    //res.setHeader('Content-Type','text/plain');
-
-    //makes the response sent as html
-    
+    console.log("WRITE Done");
 });
 
+//this works
+//console.log(user);
 
-//listens incoming requests
-//handles multiple req
-server.listen(5000); 
+// doesn't work since it can be accessed only in browser
+//alert(user);
