0e7a582d109ab7bca914f8aa16dbdcb4e3b1abe3
Barath Gopinath <barathgopi1699@gmail.com>
2020-10-25 12:01:25 +0530

    postgre-js

--- postgre-js/index.js
+++ postgre-js/index.js
@@ -1,4 +1,6 @@
+//getting Client from the pg library for postgre using node
 const {Client} = require('pg');
+//providing the credentials for the connection 
 const client = new Client({
     user : "postgres",
     password : "barathpriya83",
@@ -7,6 +9,10 @@ const client = new Client({
     database : "postgres",
 })
 
+//.connect will connect to the database 
+//using a chain of .then() to proceed each step after it fulfill's prev step's promise and execution of the step
+//this method doesn't need a try catch block it doesn't use async 
+//it will be executed in a chain manner one after another
 client.connect()
 .then(() => console.log("Connected successfully"))
 .then(() => client.query("select * from employees where name = $1",["Barath"]))
