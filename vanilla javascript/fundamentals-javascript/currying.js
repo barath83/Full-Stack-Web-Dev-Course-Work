ff1b59c11b56a8345cd64f6a0a65f0f7e6da15ce
Barath Gopinath <barathgopi1699@gmail.com>
2020-10-30 16:06:53 +0530

    currying

---
+++ vanilla javascript/fundamentals-javascript/currying.js
@@ -0,0 +1,33 @@
+/**
+  Currying in js is done using closures
+ */
+
+/* var add = function(a) {
+     return function(b){
+         return a+b;
+     }
+ };
+
+ var addToFive = add(5);
+
+ console.log(addToFive(1));
+
+ */
+
+ var avg = function(...n){
+     let tot=0;
+     for(let i=0;i<n.length;i+=1){
+         tot+=n[i];
+     }
+     return tot/n.length;
+ }
+
+ var spiceUp = function(fn,...n){
+     return function(...m){
+
+        return fn.apply(this,n.concat(m));
+     }
+ }
+
+ var doAvg = spiceUp(avg,1,2,3);
+ console.log(doAvg(4,5,6));
