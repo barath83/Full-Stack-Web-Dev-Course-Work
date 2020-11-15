c307bafa20f144dd3d75fd5a03358401d1614a01
Barath Gopinath <barathgopi1699@gmail.com>
2020-11-15 23:02:15 +0530

    Update db.js

--- pern_stack/studentForm/server/db.js
+++ pern_stack/studentForm/server/db.js
@@ -1,11 +1,11 @@
 const {Pool} = require('pg')
 
 const client = new Pool({
-    user : "postgres",
-    password : "barathpriya83",
-    host: "localhost",
-    port : 5432,
-    database : "postgres",
+    user : "",
+    password : "",
+    host: "",
+    port : ,
+    database : "",
 });
 
 
